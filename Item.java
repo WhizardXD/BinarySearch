33182193dcbc63966a8e289aedc3fd7bf289195c
WhizardXD <33294017+WhizardXD@users.noreply.github.com>
2019-05-30 14:02:04 -0700

    finished BinarySearch

--- Item.java
+++ Item.java
@@ -1,11 +1,9 @@
-public class Item
-{
+public class Item {
     private int myCatNum, myInventory;
-    public Item(int nNum, int nInv)
-    {
+    public Item(int nNum, int nInv) {
         myCatNum = nNum;
         myInventory = nInv;
     }
-    public int getCatNum(){return myCatNum;}
-    public int getInventory(){return myInventory;}
+    public int getCatNum() {return myCatNum;}
+    public int getInventory() {return myInventory;}
 }
